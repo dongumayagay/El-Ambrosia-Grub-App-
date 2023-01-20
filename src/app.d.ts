9864113ab89d6be85415f9fe19cb219118b82c3e
Don Gumayagay <dngumayagay@gmail.com>
2023-01-20 15:20:35 +0800

    setup supabase typescript

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,18 @@
+/// <reference types="@sveltejs/kit" />
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
-declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface Platform {}
+declare namespace App {
+	interface Supabase {
+		Database: import('./DatabaseDefinitions').Database
+		SchemaName: 'public'
 	}
-}
 
-export {};
+	// interface Locals {}
+	interface PageData {
+		session: import('@supabase/supabase-js').Session | null
+	}
+	// interface Error {}
+	// interface Platform {}
+}
