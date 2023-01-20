68dcb86bb08745709e4a759a6d906d39c00016ea
Don Gumayagay <dngumayagay@gmail.com>
2023-01-21 00:25:57 +0800

    test typescript

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,15 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit'
+
 // import { env } from '$env/dynamic/public'
 // or use the static env
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+import type { Database } from '$lib/types/schema';
 
-// export const supabaseClient = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY)
-export const supabase = createClient(
+
+// export const supabase = createClient<Database>(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY)
+export const supabase = createClient<Database>(
     PUBLIC_SUPABASE_URL,
     PUBLIC_SUPABASE_ANON_KEY
 )
+
+// export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
