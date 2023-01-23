d9bc4bf13102d066833865999e78ddc88673d2ca
Don Gumayagay <dngumayagay@gmail.com>
2023-01-23 12:52:29 +0800

    client navigation auth guard

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,24 @@
 import type { LayoutLoad } from './$types'
 import { getSupabase } from '@supabase/auth-helpers-sveltekit'
+import { getUserRole } from '$lib/db'
+import { error } from '@sveltejs/kit'
+
 
 export const load: LayoutLoad = async (event) => {
-    const { session } = await getSupabase(event)
-    return { session }
+
+    const { session, supabaseClient } = await getSupabase(event)
+
+    const role = session ? await getUserRole(supabaseClient, session.user.id) : null
+
+    // console.log(session, role)
+    if (
+        (event.url.pathname.startsWith('/account') && !session)
+        ||
+        event.url.pathname.startsWith('/admin') && !session) {
+        throw error(401, '/')
+    }
+
+    return { session, role }
+
 }
+
