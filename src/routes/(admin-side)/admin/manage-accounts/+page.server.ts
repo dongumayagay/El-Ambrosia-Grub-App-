9565f03fee76a93d8b4faba90790d51d9de32e1f
Don Gumayagay <dngumayagay@gmail.com>
2023-01-24 13:32:13 +0800

    table of users

--- src/routes/(admin-side)/admin/manage-accounts/+page.server.ts
+++ src/routes/(admin-side)/admin/manage-accounts/+page.server.ts
@@ -2,6 +2,5 @@ import type { PageServerLoad } from './$types';
 
 export const load = (async ({ locals }) => {
     const { data: { users }, error: err } = await locals.supabaseAdmin.auth.admin.listUsers()
-
     return { users };
 }) satisfies PageServerLoad;
--- src/routes/(admin-side)/admin/manage-accounts/+page.svelte
+++ src/routes/(admin-side)/admin/manage-accounts/+page.svelte
@@ -5,11 +5,33 @@
 	let users = data.users;
 </script>
 
-{#each users as user}
-	<div>
-		<pre>
-
-		{JSON.stringify(user, null, 2)}
-	</pre>
+<main class="p-4 min-h-full">
+	<div class="overflow-x-auto">
+		<table class="table">
+			<thead>
+				<tr>
+					<th>User ID</th>
+					<th>Email</th>
+					<th>Phone</th>
+					<th>Created at</th>
+					<th>Last sign in</th>
+					<th />
+				</tr>
+			</thead>
+			<tbody>
+				<tr class="hover">
+					{#each users as user}
+						<td>{user.id}</td>
+						<td>{user.email}</td>
+						<td>{user.phone}</td>
+						<td>{user.created_at}</td>
+						<td>{user.last_sign_in_at}</td>
+						<td>
+							<a class="btn" href={`/admin/manage-accounts/${user.id}`}>Details</a>
+						</td>
+					{/each}
+				</tr>
+			</tbody>
+		</table>
 	</div>
-{/each}
+</main>
