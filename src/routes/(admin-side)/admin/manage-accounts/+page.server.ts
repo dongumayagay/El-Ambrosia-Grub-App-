e3b03a0b56bbe26678eefdbf352bb3cb2fce731e
Don Gumayagay <dngumayagay@gmail.com>
2023-01-24 15:18:42 +0800

    optimize

--- src/routes/(admin-side)/admin/manage-accounts/+page.server.ts
+++ src/routes/(admin-side)/admin/manage-accounts/+page.server.ts
@@ -2,7 +2,7 @@ import type { PageServerLoad } from './$types';
 
 export const load = (async ({ locals }) => {
     // const { data: { users }, error: err } = await locals.supabaseAdmin.auth.admin.listUsers()
-    const { data } = await locals.supabaseClient.from("profiles").select('*')
+    const { data } = await locals.supabaseClient.from("profiles").select('id,email_address,phone_number,first_name,last_name')
 
     return { users: data ?? [] };
 }) satisfies PageServerLoad;
