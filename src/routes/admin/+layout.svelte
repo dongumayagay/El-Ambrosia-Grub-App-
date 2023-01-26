df00843102ba63eb7dccc417290041fb68fed49b
Don Gumayagay <dngumayagay@gmail.com>
2023-01-27 03:35:14 +0800

    change user page

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,3 +1,3 @@
-<main class="flex-1 flex flex-col p-4 gap-4">
+<main class="flex-1 flex flex-col p-4 gap-4 ">
 	<slot />
 </main>
--- src/routes/admin/accounts/[user_id]/+page.svelte
+++ src/routes/admin/accounts/[user_id]/+page.svelte
@@ -8,15 +8,66 @@
 	const user = data.user_info;
 </script>
 
-<h1>User Information</h1>
-<ul>
-	<li>ID: {user.id}</li>
-	<li>Email: {user.email_address}</li>
-	<li>Phone: {user.phone_number}</li>
-	<li>Created at: {datetime_formatter(user.created_at)}</li>
-	<li>Last sign in: {datetime_formatter(user.last_sign_in_at)}</li>
-	<li>First name: {user.first_name}</li>
-	<li>Last name: {user.last_name}</li>
-	<li>Role: {user.position}</li>
-</ul>
+<div class="prose w-full max-w-md grid sm:grid-cols-2 gap-2">
+	<h1 class="col-span-full">User Information</h1>
+	<div class=" col-span-full form-control ">
+		<label class="label" for="">
+			<span class="label-text">UID</span>
+		</label>
+		<input type="text" disabled class="input input-bordered cursor-text" value={user.id} />
+	</div>
+	<div class="form-control ">
+		<label class="label" for="">
+			<span class="label-text">Email address</span>
+		</label>
+		<input type="text" class="input input-bordered" disabled value={user.email_address} />
+	</div>
+	<div class="form-control ">
+		<label class="label" for="">
+			<span class="label-text">Phone number</span>
+		</label>
+		<input type="text" class="input input-bordered" disabled value={user.phone_number} />
+	</div>
+	<div class="form-control ">
+		<label class="label" for="">
+			<span class="label-text">Created at</span>
+		</label>
+		<input
+			type="text"
+			class="input input-bordered"
+			disabled
+			value={datetime_formatter(user.created_at)}
+		/>
+	</div>
+	<div class="form-control ">
+		<label class="label" for="">
+			<span class="label-text">Last sign in</span>
+		</label>
+		<input
+			type="text"
+			class="input input-bordered"
+			disabled
+			value={datetime_formatter(user.last_sign_in_at)}
+		/>
+	</div>
+	<div class="form-control ">
+		<label class="label" for="">
+			<span class="label-text">First name</span>
+		</label>
+		<input type="text" class="input input-bordered" disabled value={user.first_name} />
+	</div>
+	<div class="form-control ">
+		<label class="label" for="">
+			<span class="label-text">Last name</span>
+		</label>
+		<input type="text" class="input input-bordered" disabled value={user.last_name} />
+	</div>
+	<div class="form-control ">
+		<label class="label" for="">
+			<span class="label-text">Role / Position</span>
+		</label>
+		<input type="text" class="input input-bordered" disabled value={user.position} />
+	</div>
+	<!-- {user.} -->
+</div>
 <ChangeRole {form} user_id={user.id ?? ''} />
