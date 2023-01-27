46a597505f905f72e6bf89fc67c7816180eb4a7f
Don Gumayagay <dngumayagay@gmail.com>
2023-01-27 13:25:06 +0800

    tweak auth styles

---
+++ src/routes/auth/+layout.svelte
@@ -0,0 +1,7 @@
+<div class=" h-full flex flex-col justify-center items-center px-4">
+	<main class="flex flex-col w-full max-w-md text-center pb-20">
+		<slot />
+		<div class="divider">or</div>
+		<button class="btn btn-outline"> Sign in with Google </button>
+	</main>
+</div>
--- src/routes/auth/register/+page.svelte
+++ src/routes/auth/register/+page.svelte
@@ -15,30 +15,31 @@
 	};
 </script>
 
-<main class=" h-full flex flex-col justify-center items-center">
-	<h1 class="font-bold text-3xl uppercase">Register</h1>
-	<form method="post" use:enhance={enhance_function} class="flex flex-col w-full max-w-sm">
-		<div class="form-control">
-			<label class="label" for="email">
-				<span class="label-text">Email Address</span>
-			</label>
-			<input type="email" name="email" placeholder="Email Address" class="input input-bordered" />
-		</div>
-		<div class="form-control">
-			<label class="label" for="password">
-				<span class="label-text">Password</span>
-			</label>
-			<input type="password" name="password" placeholder="Password" class="input input-bordered" />
-		</div>
+<form method="post" use:enhance={enhance_function} class="">
+	<div class="prose">
+		<h2 class="uppercase">Register</h2>
+	</div>
+	<div class="form-control">
+		<label class="label" for="email">
+			<span class="label-text">Email Address</span>
+		</label>
+		<input type="email" name="email" placeholder="Email Address" class="input input-bordered" />
+	</div>
+	<div class="form-control">
+		<label class="label" for="password">
+			<span class="label-text">Password</span>
+		</label>
+		<input type="password" name="password" placeholder="Password" class="input input-bordered" />
+	</div>
+	<br />
+	<button class="btn btn-block" class:loading disabled={loading}>create my account</button>
+	<br />
+	{#if form?.error}
 		<br />
-		<button class="btn" class:loading disabled={loading}>create my account</button>
-		<br />
-		{#if form?.error}
-			<p class="text-center text-error font-bold ">
-				{form.error}
-			</p>
-			<br />
-		{/if}
-		<a class="text-center" href="/auth/login">Already have and account? Login</a>
-	</form>
-</main>
+		<p class="text-center text-error font-bold ">
+			{form.error}
+		</p>
+	{/if}
+	<br />
+	<a class="text-center" href="/auth/login">Already have and account? Login</a>
+</form>
