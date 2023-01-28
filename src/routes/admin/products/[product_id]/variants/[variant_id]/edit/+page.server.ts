28570a87dfb8e5cc324ab466aeb7d561a176fde3
Don Gumayagay <dngumayagay@gmail.com>
2023-01-28 09:39:52 +0800

    update product variant

--- src/routes/admin/products/[product_id]/variants/[variant_id]/edit/+page.server.ts
+++ src/routes/admin/products/[product_id]/variants/[variant_id]/edit/+page.server.ts
@@ -1,5 +1,26 @@
-import type { PageServerLoad } from './$types';
+import { fail, redirect } from '@sveltejs/kit';
+import type { PageServerLoad, Actions } from './$types';
 
-export const load = (async () => {
-    return {};
+export const load = (async ({ locals, params }) => {
+
+    return {
+        variant: await (await locals.supabaseClient.from("product_variants").select('*').eq("id", params.variant_id).limit(1).single()).data,
+    };
 }) satisfies PageServerLoad;
+
+export const actions: Actions = {
+    default: async ({ request, locals, params }) => {
+        const body = Object.fromEntries(await request.formData())
+
+        const product_variant = {
+            name: body.name.toString(),
+            price: Number(body.price.toString())
+        }
+
+
+        const { error: err } = await locals.supabaseClient.from('product_variants').update(product_variant).eq('id', Number(body.variant_id.toString()))
+        if (err)
+            return fail(400, { error: err.message })
+        throw redirect(303, `/admin/products/${params.product_id}/variants`)
+    }
+};
--- src/routes/admin/products/[product_id]/variants/[variant_id]/edit/+page.svelte
+++ src/routes/admin/products/[product_id]/variants/[variant_id]/edit/+page.svelte
@@ -1,5 +1,60 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	import { enhance, type SubmitFunction } from '$app/forms';
+	import NotFound from '$lib/components/NotFound.svelte';
+	import type { PageData, ActionData } from './$types';
 
+	export let form: ActionData;
 	export let data: PageData;
+	const { variant } = data;
+	let loading: boolean;
+
+	const enhance_function: SubmitFunction = () => {
+		loading = true;
+		return async ({ update }) => {
+			loading = false;
+			await update();
+		};
+	};
 </script>
+
+{#if variant}
+	<form method="post" use:enhance={enhance_function} class="w-full max-w-md prose">
+		<h2 class="uppercase">Update variant for {variant.name}</h2>
+		<input type="hidden" name="variant_id" value={variant.id} />
+		<div class="form-control">
+			<label class="label" for="name">
+				<span class="label-text">Name</span>
+			</label>
+			<input
+				type="text"
+				name="name"
+				placeholder="regular / chicken / steak / etc"
+				class="input input-bordered "
+				value={variant.name}
+				required
+			/>
+		</div>
+		<div class="form-control">
+			<label class="label" for="price">
+				<span class="label-text">Price</span>
+			</label>
+			<input
+				type="number"
+				name="price"
+				placeholder="100"
+				class="input input-bordered "
+				required
+				value={variant.price}
+			/>
+		</div>
+
+		<br />
+		<button disabled={loading} class="btn btn-block" class:loading>add variant</button>
+		<br />
+		{#if form?.error}
+			<p class="text-center text-error font-bold">{form.error}</p>
+		{/if}
+	</form>
+{:else}
+	<NotFound />
+{/if}
