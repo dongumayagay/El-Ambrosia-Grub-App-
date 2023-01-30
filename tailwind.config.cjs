56bcf49339e90167aa96e183a4b32a941805fffe
Don Gumayagay <dngumayagay@gmail.com>
2023-01-31 04:40:01 +0800

    add branch chuchu

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				'brand-red': '#a80103',
+				'brand-maroon': '#4b200b',
+				'brand-tan': '#f7dd9d'
+			}
+		}
 	},
 	plugins: [require('daisyui'), require('@tailwindcss/typography'), require('@tailwindcss/forms')]
 };
