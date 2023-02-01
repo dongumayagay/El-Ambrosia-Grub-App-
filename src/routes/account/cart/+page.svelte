5947e63439e9a8819f82efd6200ee86201ea65fb
Don Gumayagay <dngumayagay@gmail.com>
2023-02-01 12:14:01 +0800

    front end

--- src/routes/account/cart/+page.svelte
+++ src/routes/account/cart/+page.svelte
@@ -9,9 +9,13 @@
 {#if cart_items}
 	<div class="overflow-x-auto">
 		<table class="table w-full">
-			<!-- head -->
 			<thead>
-				<tr>
+				<!-- small screen -->
+				<tr class="sm:hidden">
+					<th>Item</th>
+				</tr>
+				<!-- medium to large screen -->
+				<tr class="hidden sm:table-row">
 					<th>Item</th>
 					<th>Quantity</th>
 					<th>Price</th>
@@ -20,18 +24,68 @@
 			</thead>
 			<tbody>
 				{#each cart_items as item}
-					<!-- row 1 -->
-					<tr>
-						<td>
-							<!-- <div class="avatar">
-								<div class=" w-32 rounded">
-									<img
-										src={display_property(item.products, 'image_url')}
-										alt=""
-										class="object-cover"
-									/>
+					<!-- small screen -->
+					<tr class="sm:hidden">
+						<td class="">
+							<div class="flex gap-2">
+								<figure class="avatar">
+									<div class=" w-28 h-28 rounded">
+										<img
+											src={display_property(item.products, 'image_url')}
+											alt={`${display_property(item.product_variants, 'name')} ${display_property(
+												item.products,
+												'name'
+											)}`}
+											class="object-cover"
+										/>
+									</div>
+								</figure>
+								<div class="w-full">
+									<article class="prose ">
+										<h4>
+											{display_property(item.product_variants, 'name')}
+											{display_property(item.products, 'name')}
+										</h4>
+										<h5>₱ {display_property(item.product_variants, 'price')}</h5>
+									</article>
+									<div class="flex justify-between">
+										<form action="?/change_quantity" method="post" class="contents">
+											<input type="hidden" name="cart_item_id" value={item.id} />
+											<input
+												type="number"
+												name="quantity"
+												value={item.quantity}
+												class="input input-bordered w-32 text-center"
+											/>
+											<button class="hidden" />
+										</form>
+										<form action="?/remove_cart_item" method="post" class="contents">
+											<input type="hidden" name="cart_item_id" value={item.id} />
+											<button class="btn btn-ghost btn-square"
+												><svg
+													xmlns="http://www.w3.org/2000/svg"
+													fill="none"
+													viewBox="0 0 24 24"
+													stroke-width="1.5"
+													stroke="currentColor"
+													class="w-6 h-6"
+												>
+													<path
+														stroke-linecap="round"
+														stroke-linejoin="round"
+														d="M6 18L18 6M6 6l12 12"
+													/>
+												</svg></button
+											>
+										</form>
+									</div>
 								</div>
-							</div> -->
+							</div>
+						</td>
+					</tr>
+					<!-- medium to large screen -->
+					<tr class="hidden sm:table-row">
+						<td class="w-full max-w-sm">
 							<div class="flex gap-4">
 								<figure class="avatar">
 									<div class=" w-32 rounded">
@@ -42,28 +96,46 @@
 										/>
 									</div>
 								</figure>
-								<div>
-									<div class="font-bold">
+								<article class="prose">
+									<h2>
 										{display_property(item.product_variants, 'name')}
 										{display_property(item.products, 'name')}
-									</div>
-									<div class="text-sm opacity-50">United States</div>
-								</div>
+									</h2>
+									<p class=" break-all whitespace-normal ">
+										{display_property(item.products, 'description')}
+									</p>
+								</article>
 							</div>
 						</td>
-						<td>{item.quantity}</td>
-						<td>₱ {display_property(item.product_variants, 'price')}</td>
 						<td
-							><svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke-width="1.5"
-								stroke="currentColor"
-								class="w-6 h-6"
-							>
-								<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-							</svg>
+							><form action="?/change_quantity" method="post" class="contents">
+								<input type="hidden" name="cart_item_id" value={item.id} />
+								<input
+									type="number"
+									name="quantity"
+									value={item.quantity}
+									class="input input-bordered w-32 text-center"
+								/>
+								<button class="hidden" />
+							</form></td
+						>
+						<td>₱ {display_property(item.product_variants, 'price')}</td>
+						<td>
+							<form action="?/remove_cart_item" method="post" class="contents">
+								<input type="hidden" name="cart_item_id" value={item.id} />
+								<button class="btn btn-ghost btn-square"
+									><svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke-width="1.5"
+										stroke="currentColor"
+										class="w-6 h-6"
+									>
+										<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+									</svg></button
+								>
+							</form>
 						</td>
 					</tr>
 				{:else}
