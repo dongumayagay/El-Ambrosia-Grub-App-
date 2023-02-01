0953aa8a5936db488add888af12b8fb195b1947c
Don Gumayagay <dngumayagay@gmail.com>
2023-02-02 05:03:57 +0800

    orders page

--- src/routes/account/orders/[order_id]/+page.server.ts
+++ src/routes/account/orders/[order_id]/+page.server.ts
@@ -3,6 +3,6 @@ import type { PageServerLoad } from './$types';
 export const load = (async ({ locals, params }) => {
     return {
         order: await (await locals.supabaseClient.from('orders').select('*').eq('id', params.order_id).limit(1).single()).data,
-        order_items: await (await locals.supabaseClient.from('order_items').select('*,products(*)').eq('order_id', params.order_id)).data
+        order_items: await (await locals.supabaseClient.from('order_items').select('*,products(*),product_variants(*)').eq('order_id', params.order_id)).data
     };
 }) satisfies PageServerLoad;
--- src/routes/account/orders/[order_id]/+page.svelte
+++ src/routes/account/orders/[order_id]/+page.svelte
@@ -1,13 +1,138 @@
 <script lang="ts">
+	import { Order_States } from '$lib/constants';
+	import { datetime_formatter, display_property } from '$lib/utils';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+
+	const states = Object.keys(Order_States).filter((v) => isNaN(Number(v)));
+	$: order_items = data.order_items ?? [];
 </script>
 
-<pre class=" prose">
+{#if data.order}
+	<main class="flex flex-col sm:flex-col-reverse py-8">
+		<section>
+			<div class="form-control w-full">
+				<label for="" class="label">
+					<span class="label-text">Date Ordered</span>
+				</label>
+				<input
+					type="text"
+					value={datetime_formatter(data.order.created_at)}
+					class="input input-bordered w-full"
+					disabled
+				/>
+			</div>
+			<div class="form-control w-full">
+				<label for="" class="label">
+					<span class="label-text">Total</span>
+				</label>
+				<input
+					type="text"
+					value={`₱${data.order.total}`}
+					class="input input-bordered w-full"
+					disabled
+				/>
+			</div>
+			<br />
+			{#if data.order.status === 0}
+				<form class="form-control" method="get">
+					<button class="btn">PAY</button>
+				</form>
+			{/if}
+		</section>
+		<section class="flex sm:justify-center">
+			<ul class="steps steps-vertical sm:steps-horizontal">
+				{#each states as state, index}
+					<li class="step uppercase" class:step-primary={data.order.status >= index}>{state}</li>
+				{/each}
+			</ul>
+		</section>
+	</main>
+
+	<!-- <pre class=" prose">
 {JSON.stringify(data.order, null, 2)}
 </pre>
 
-<pre class=" prose">
+	<pre class=" prose">
 {JSON.stringify(data.order_items, null, 2)}
-</pre>
+</pre> -->
+	<div class="overflow-x-auto">
+		<table class="table w-full">
+			<thead>
+				<!-- small screen -->
+				<tr class="sticky top-0 sm:hidden">
+					<th>Item</th>
+				</tr>
+				<!-- medium to large screen -->
+				<tr class="sticky top-0 hidden sm:table-row">
+					<th>Item</th>
+					<th>Quantity</th>
+					<th>Price</th>
+					<th />
+				</tr>
+			</thead>
+			<tbody>
+				{#each order_items as item (item.id)}
+					<!-- small screen -->
+					<tr class="sm:hidden">
+						<td class="">
+							<div class="flex gap-2">
+								<figure class="avatar">
+									<div class=" w-28 h-28 rounded">
+										<img
+											src={display_property(item.products, 'image_url')}
+											alt={`${display_property(item.product_variants, 'name')} ${display_property(
+												item.products,
+												'name'
+											)}`}
+											class="object-cover"
+										/>
+									</div>
+								</figure>
+								<div class="w-full">
+									<article class="prose ">
+										<h4>
+											{display_property(item.product_variants, 'name')}
+											{display_property(item.products, 'name')}
+										</h4>
+										<h5>₱ {display_property(item.product_variants, 'price')}</h5>
+										<h5>Qty: {item.quantity}</h5>
+									</article>
+								</div>
+							</div>
+						</td>
+					</tr>
+					<!-- medium to large screen -->
+					<tr class="hidden sm:table-row">
+						<td class="w-full max-w-sm">
+							<div class="flex gap-4">
+								<figure class="avatar">
+									<div class=" w-32 rounded">
+										<img
+											src={display_property(item.products, 'image_url')}
+											alt=""
+											class="object-cover"
+										/>
+									</div>
+								</figure>
+								<article class="prose">
+									<h2>
+										{display_property(item.product_variants, 'name')}
+										{display_property(item.products, 'name')}
+									</h2>
+									<p class=" break-all whitespace-normal ">
+										{display_property(item.products, 'description')}
+									</p>
+								</article>
+							</div>
+						</td>
+						<td>{item.quantity}</td>
+						<td>₱ {display_property(item.product_variants, 'price')}</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+		<br />
+	</div>
+{/if}
