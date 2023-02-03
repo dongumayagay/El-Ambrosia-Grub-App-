12d4afc1832d1996dec47795ca1bed047a4c675d
Don Gumayagay <dngumayagay@gmail.com>
2023-02-03 22:12:44 +0800

    total and persist

--- src/lib/stores/cart.ts
+++ src/lib/stores/cart.ts
@@ -1,14 +1,36 @@
-import { writable } from 'svelte/store';
+import { browser } from '$app/environment';
+import { writable, derived } from 'svelte/store';
 
 
 type CartItem = {
     variant_id: number,
     quantity: number,
+    name: string,
+    image_url: string,
+    price: number,
 }
 
 
 const create_cart = () => {
-    const { set, subscribe, update } = writable<CartItem[]>([])
+    const localStorageKey = 'cart'
+    const cart = writable<CartItem[]>(get_cart_from_localStorage())
+    const subtotal = derived(cart, ($cart) =>
+        $cart.reduce((total, item) =>
+            total + item.quantity * item.price, 0)
+    );
+
+    const { set, subscribe, update } = cart
+
+    subscribe((items) =>
+        localStorage.setItem(localStorageKey, JSON.stringify(items))
+    )
+
+    function get_cart_from_localStorage() {
+        if (!browser) return []
+        const cart = localStorage.getItem(localStorageKey)
+        if (!cart) return []
+        return JSON.parse(cart)
+    }
 
     function add(item: CartItem) {
         update(items => {
@@ -41,12 +63,15 @@ const create_cart = () => {
         set([])
     }
 
+
+
     return {
         subscribe,
         add,
         updateItemQuantity,
         removeItem,
-        clear
+        clear,
+        subtotal
     }
 }
 
