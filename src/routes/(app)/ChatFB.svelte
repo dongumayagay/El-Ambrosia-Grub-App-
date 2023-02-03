0f5cd7f868f8ffe2f3de0e88e2699579df78a484
Don Gumayagay <dngumayagay@gmail.com>
2023-02-03 09:25:44 +0800

    disable fb chat on dev mode

--- src/routes/(app)/ChatFB.svelte
+++ src/routes/(app)/ChatFB.svelte
@@ -1,54 +1,41 @@
 <script lang="ts">
 	// @ts-nocheck
+	import { dev } from '$app/environment';
 	import { onMount } from 'svelte';
 
-	onMount(() => {
-		window.fbAsyncInit = function () {
-			FB.init({
-				xfbml: true,
-				version: 'v16.0'
-			});
-		};
+	if (!dev) {
+		onMount(() => {
+			window.fbAsyncInit = function () {
+				FB.init({
+					xfbml: true,
+					version: 'v16.0'
+				});
+			};
 
-		(function (d, s, id) {
-			var js,
-				fjs = d.getElementsByTagName(s)[0];
-			if (d.getElementById(id)) return;
-			js = d.createElement(s);
-			js.id = id;
-			js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-			fjs.parentNode.insertBefore(js, fjs);
-		})(document, 'script', 'facebook-jssdk');
-	});
+			(function (d, s, id) {
+				var js,
+					fjs = d.getElementsByTagName(s)[0];
+				if (d.getElementById(id)) return;
+				js = d.createElement(s);
+				js.id = id;
+				js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+				fjs.parentNode.insertBefore(js, fjs);
+			})(document, 'script', 'facebook-jssdk');
+		});
+	}
 </script>
 
-<!-- Messenger Chat plugin Code -->
-<div id="fb-root" />
+{#if !dev}
+	<!-- Messenger Chat plugin Code -->
+	<div id="fb-root" />
 
-<!-- Your Chat plugin code -->
-<div
-	id="fb-customer-chat"
-	class="fb-customerchat"
-	page_id={'116061764725071'}
-	attribution={'biz_inbox'}
-/>
+	<!-- Your Chat plugin code -->
+	<div
+		id="fb-customer-chat"
+		class="fb-customerchat"
+		page_id={'116061764725071'}
+		attribution={'biz_inbox'}
+	/>
 
-<!-- Your SDK code -->
-<!-- <script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      xfbml            : true,
-      version          : 'v16.0'
-    });
-  };
-
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-</script> -->
-
-<svelte:element this="script" />
+	<svelte:element this="script" />
+{/if}
