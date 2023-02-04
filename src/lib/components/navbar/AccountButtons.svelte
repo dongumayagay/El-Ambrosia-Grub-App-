75fe2f3537308ee7313ccaeec26f73c94a32e2ea
Don Gumayagay <dngumayagay@gmail.com>
2023-02-04 08:29:01 +0800

    icon size

--- src/lib/components/navbar/AccountButtons.svelte
+++ src/lib/components/navbar/AccountButtons.svelte
@@ -8,16 +8,16 @@
 		<section class="dropdown dropdown-end dropdown-hover ">
 			<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
 			<!-- svelte-ignore a11y-label-has-associated-control -->
-			<label tabindex="0" class="btn btn-square btn-ghost"
-				><svg
+			<label tabindex="0" class="btn btn-square btn-ghost text-brand-tan">
+				<svg
 					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 24 24"
+					viewBox="0 0 20 20"
 					fill="currentColor"
-					class="w-6 h-6 fill-accent"
+					class="w-5 h-5"
 				>
 					<path
 						fill-rule="evenodd"
-						d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+						d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-5.5-2.5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zM10 12a5.99 5.99 0 00-4.793 2.39A6.483 6.483 0 0010 16.5a6.483 6.483 0 004.793-2.11A5.99 5.99 0 0010 12z"
 						clip-rule="evenodd"
 					/>
 				</svg>
