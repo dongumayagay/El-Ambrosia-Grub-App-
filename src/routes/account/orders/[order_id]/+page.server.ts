b2553ce9c8b0921faa25467b7ca85f7b58063909
Don Gumayagay <dngumayagay@gmail.com>
2023-02-06 19:47:13 +0800

    update order status

--- src/routes/account/orders/[order_id]/+page.server.ts
+++ src/routes/account/orders/[order_id]/+page.server.ts
@@ -3,6 +3,19 @@ import { error } from '@sveltejs/kit';
 import type { PageServerLoad, Actions } from './$types';
 
 export const load = (async ({ locals, params }) => {
+
+    const { data: order, error: err_order } = await locals.supabaseClient.from('orders').select('*').eq('id', params.order_id).limit(1).single()
+    if (!order || err_order) throw error(404, JSON.stringify(err_order, null, 2))
+    if (order.status === 0 && order.invoice_id !== null) {
+        const resp = await i.getInvoice({ invoiceID: order.invoice_id })
+        // @ts-ignore
+        if (resp.status === 'PAID' || resp.status === 'SETTLED') {
+            const { error: err_update_order_status } = await locals.supabaseClient.from('orders').update({ status: 1 }).eq('id', order.id)
+            console.log(err_update_order_status)
+            throw error(500, JSON.stringify(err_update_order_status))
+        }
+    }
+
     return {
         order: await (await locals.supabaseClient.from('orders').select('*').eq('id', params.order_id).limit(1).single()).data,
         order_items: await (await locals.supabaseClient.from('order_items').select('*,products(*),product_variants(*)').eq('order_id', params.order_id)).data
