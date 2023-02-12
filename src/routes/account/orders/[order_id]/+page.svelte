b7e387d3402cf0367bf4ea0ae0260c283e560890
Don Gumayagay <dngumayagay@gmail.com>
2023-02-12 11:04:29 +0800

    progressive enhancement payment

--- src/routes/account/orders/[order_id]/+page.svelte
+++ src/routes/account/orders/[order_id]/+page.svelte
@@ -1,18 +1,28 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	import { enhance } from '$app/forms';
+	import { enhance, type SubmitFunction } from '$app/forms';
+
 	import OrderDetails from '$lib/components/OrderDetails.svelte';
 
 	export let data: PageData;
+	let loading: boolean;
+
+	const enhance_function: SubmitFunction = () => {
+		loading = true;
+		return async ({ update }) => {
+			await update();
+			loading = false;
+		};
+	};
 </script>
 
 {#if data.order && data.order_items}
 	<main>
 		<OrderDetails {data} />
 		{#if data.order.status === 0}
-			<form action="?/pay" method="post" class="gap-4 flex flex-col" use:enhance>
-				<button class="btn btn-block">Pay</button>
-				<button formaction="?/cancel" class="btn btn-block btn-outline">Cancel</button>
+			<form action="?/pay" method="post" class="gap-4 flex flex-col" use:enhance={enhance_function}>
+				<button class="btn btn-block" class:loading disabled={loading}>Pay</button>
+				<button formaction="?/cancel" class="btn btn-block btn-outline ">Cancel</button>
 			</form>
 		{/if}
 	</main>
