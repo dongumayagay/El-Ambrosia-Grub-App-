64451c20b339889e17340b539b6f42b2d2b15cf3
Don Gumayagay <dngumayagay@gmail.com>
2023-02-12 22:34:07 +0800

    test webhook

---
+++ src/routes/api/invoice_callback_url/+server.ts
@@ -0,0 +1,15 @@
+import { XENDIT_CALLBACK_VERIFICATION_VERIFICATION_TOKEN } from '$env/static/private';
+import { error } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+
+export const POST: RequestHandler = async ({ request, locals }) => {
+    const x_callback_token = request.headers.get('x-callback-token')
+    if (!x_callback_token || x_callback_token !== XENDIT_CALLBACK_VERIFICATION_VERIFICATION_TOKEN)
+        throw error(400)
+    const data = await request.json()
+    if (data.status === 'PAID') {
+        const { error: err } = await locals.supabaseClient.rpc('order_next_status', { order_id: Number(data.external_id) })
+        if (err) throw error(500, err)
+    }
+    return new Response();
+};
