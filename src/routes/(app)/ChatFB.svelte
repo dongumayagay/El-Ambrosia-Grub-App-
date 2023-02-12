b563f8e7e4152e7ca0a0f16d5f0435c05fe33fe4
Don Gumayagay <dngumayagay@gmail.com>
2023-02-12 10:30:28 +0800

    chat fb only in prod

--- src/routes/(app)/ChatFB.svelte
+++ src/routes/(app)/ChatFB.svelte
@@ -1,36 +1,38 @@
 <script lang="ts">
-	import { browser } from '$app/environment';
+	import { browser, dev } from '$app/environment';
 </script>
 
-<!-- Messenger Chat plugin Code -->
-<div id="fb-root" />
+{#if !dev}
+	<!-- Messenger Chat plugin Code -->
+	<div id="fb-root" />
 
-<!-- Your Chat plugin code -->
-<div id="fb-customer-chat" class="fb-customerchat" />
-{#if browser}
-	<script>
-		var chatbox = document.getElementById('fb-customer-chat');
-		chatbox.setAttribute('page_id', '107347208351582');
-		chatbox.setAttribute('attribution', 'biz_inbox');
-	</script>
+	<!-- Your Chat plugin code -->
+	<div id="fb-customer-chat" class="fb-customerchat" />
+	{#if browser}
+		<script>
+			var chatbox = document.getElementById('fb-customer-chat');
+			chatbox.setAttribute('page_id', '107347208351582');
+			chatbox.setAttribute('attribution', 'biz_inbox');
+		</script>
 
-	<!-- Your SDK code -->
-	<script>
-		window.fbAsyncInit = function () {
-			FB.init({
-				xfbml: true,
-				version: 'v16.0'
-			});
-		};
+		<!-- Your SDK code -->
+		<script>
+			window.fbAsyncInit = function () {
+				FB.init({
+					xfbml: true,
+					version: 'v16.0'
+				});
+			};
 
-		(function (d, s, id) {
-			var js,
-				fjs = d.getElementsByTagName(s)[0];
-			if (d.getElementById(id)) return;
-			js = d.createElement(s);
-			js.id = id;
-			js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-			fjs.parentNode.insertBefore(js, fjs);
-		})(document, 'script', 'facebook-jssdk');
-	</script>
+			(function (d, s, id) {
+				var js,
+					fjs = d.getElementsByTagName(s)[0];
+				if (d.getElementById(id)) return;
+				js = d.createElement(s);
+				js.id = id;
+				js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+				fjs.parentNode.insertBefore(js, fjs);
+			})(document, 'script', 'facebook-jssdk');
+		</script>
+	{/if}
 {/if}
