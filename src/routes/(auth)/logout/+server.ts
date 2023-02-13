a8ab9973a85593891db95376e7db5ffc5e07d101
Don Gumayagay <dngumayagay@gmail.com>
2023-02-13 22:41:50 +0800

    logout

--- src/routes/(auth)/logout/+server.ts
+++ src/routes/(auth)/logout/+server.ts
@@ -2,8 +2,7 @@ import { error, redirect } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const POST: RequestHandler = async ({ locals }) => {
-    console.log('logout')
-    // const { error: err } = await locals.supabaseClient.auth.signOut()
-    // if (err) throw error(500, 'Something went wrong logging  you out.');
+    const { error: err } = await locals.supabaseClient.auth.signOut()
+    if (err) throw error(500, 'Something went wrong logging  you out.');
     throw redirect(303, '/')
 };
