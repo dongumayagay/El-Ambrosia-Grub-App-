f605e6f1936caac9d2bb117e9b9040039aa2bbde
Don Gumayagay <dngumayagay@gmail.com>
2023-02-13 10:42:24 +0800

    revamp assign role

--- src/routes/admin/accounts/[user_id]/assign-role/+page.server.ts
+++ src/routes/admin/accounts/[user_id]/assign-role/+page.server.ts
@@ -1,27 +1,13 @@
-import { supabaseAdmin } from '$lib/db/admin.server';
-import { error } from '@sveltejs/kit';
-import type { PageServerLoad, Actions } from './$types';
-
-export const load = (async ({ locals, params }) => {
-    const { user_id } = params
-
-    return {
-        user_info: {
-            ...await (await locals.supabaseAdmin.auth.admin.getUserById(user_id)).data.user,
-            ...await (await supabaseAdmin.from("roles").select(`*`).eq('id', user_id).limit(1).single()).data,
-            ...await (await supabaseAdmin.from("profiles").select(`*`).eq('.id', user_id).limit(1).single()).data,
-        }
-    };
-}) satisfies PageServerLoad;
+import { fail } from '@sveltejs/kit';
+import type { Actions } from './$types';
 
 export const actions: Actions = {
-    change_role: async ({ request, locals }) => {
-        const body = Object.fromEntries(await request.formData())
-        const user_id = body.user_id as string
-        const new_role = (body.new_role === 'null') ? null : body.new_role as string
-
-        const { error: err } = await locals.supabaseClient.from('roles').update({ position: new_role }).eq('id', user_id)
-        if (err) throw error(500, err.message)
+    default: async ({ request, locals, params }) => {
+        const data = await request.formData()
+        const new_role = data.get('role')?.toString()
+        console.log(new_role)
+        const { error: err } = await locals.supabaseClient.from('roles').update({ position: new_role }).eq('id', params.user_id)
+        if (err) return fail(500, { error: JSON.stringify(err) })
         return { success: true }
     }
 };
--- src/routes/admin/accounts/[user_id]/assign-role/+page.svelte
+++ src/routes/admin/accounts/[user_id]/assign-role/+page.svelte
@@ -1,55 +1,85 @@
 <script lang="ts">
-	import { datetime_formatter } from '$lib/misc/utils';
-	import type { PageData } from '../$types';
-	import ChangeRole from './ChangeRole.svelte';
-	import type { ActionData } from '../$types';
-	export let form: ActionData;
+	import { ROLES_ALLOWED_IN_ADMIN } from '$lib/db/client';
+	import type { PageData, ActionData } from './$types';
+
 	export let data: PageData;
-	const user = data.user_info;
+	export let form: ActionData;
+
+	let submit_button: HTMLButtonElement;
 </script>
 
-<div class="prose w-full max-w-md grid sm:grid-cols-2 gap-2">
-	<div class=" col-span-full form-control ">
-		<label class="label" for=""> <span class="label-text">UID</span> </label>
-		<input type="text" readonly class="input input-bordered cursor-text" value={user.id} />
+<div class=" grid sm:grid-cols-2 gap-4">
+	<div class="form-control col-span-full">
+		<label class="label" for="">
+			<span class="label-text">User ID</span>
+		</label>
+		<input class="input input-bordered " readonly value={data.profile?.id} />
 	</div>
-	<div class="form-control ">
-		<label class="label" for=""> <span class="label-text">Email address</span> </label>
-		<input type="text" class="input input-bordered" readonly value={user.email} />
+
+	<div class="form-control col-span-full">
+		<label class="label" for="">
+			<span class="label-text">Email</span>
+		</label>
+		<input class="input input-bordered " readonly value={data.profile?.email_address} />
 	</div>
-	<div class="form-control ">
-		<label class="label" for=""> <span class="label-text">Phone number</span> </label>
-		<input type="text" class="input input-bordered" readonly value={user.phone_number} />
-	</div>
-	<div class="form-control ">
-		<label class="label" for=""> <span class="label-text">Created at</span> </label>
-		<input
-			type="text"
-			class="input input-bordered"
-			readonly
-			value={datetime_formatter(user.created_at)}
-		/>
-	</div>
-	<div class="form-control ">
-		<label class="label" for=""> <span class="label-text">Last sign in</span> </label>
-		<input
-			type="text"
-			class="input input-bordered"
-			readonly
-			value={datetime_formatter(user.last_sign_in_at)}
-		/>
-	</div>
-	<div class="form-control ">
-		<label class="label" for=""> <span class="label-text">First name</span> </label>
-		<input type="text" class="input input-bordered" readonly value={user.first_name} />
-	</div>
-	<div class="form-control ">
-		<label class="label" for=""> <span class="label-text">Last name</span> </label>
-		<input type="text" class="input input-bordered" readonly value={user.last_name} />
-	</div>
-	<div class="form-control ">
-		<label class="label" for=""> <span class="label-text">Role / Position</span> </label>
-		<input type="text" class="input input-bordered" readonly value={user.position} />
-	</div>
+	<form method="post" class="contents">
+		<div class="form-control col-span-full">
+			<label class="label" for="">
+				<span class="label-text">Role</span>
+			</label>
+			<select
+				name="role"
+				class="select select-bordered"
+				on:change={() => {
+					submit_button.click();
+				}}
+			>
+				<option value={null}>None</option>
+				{#each ROLES_ALLOWED_IN_ADMIN as role}
+					<option value={role} selected={role === data.roles?.position}
+						>{role?.replaceAll('_', ' ')}</option
+					>
+				{/each}
+			</select>
+		</div>
+		<button bind:this={submit_button} class="hidden" />
+	</form>
 </div>
-<ChangeRole {form} user_id={user.id ?? ''} />
+{#if form?.success}
+	<div class="alert alert-success shadow-lg">
+		<div>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="stroke-current flex-shrink-0 h-6 w-6"
+				fill="none"
+				viewBox="0 0 24 24"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+				/></svg
+			>
+			<span>Changed role successfully</span>
+		</div>
+	</div>
+{/if}
+{#if form?.error}
+	<div class="alert alert-error shadow-lg">
+		<div>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="stroke-current flex-shrink-0 h-6 w-6"
+				fill="none"
+				viewBox="0 0 24 24"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+				/></svg
+			>
+			<span>Error! something went wrong.</span>
+		</div>
+	</div>
+{/if}
