ceb4b4f8f7de51acba8ed436399dcf8f1fd1a091
Don Gumayagay <dngumayagay@gmail.com>
2023-02-13 11:46:16 +0800

    add product

--- src/routes/admin/products/add/+page.svelte
+++ src/routes/admin/products/add/+page.svelte
@@ -27,8 +27,7 @@
 	}
 </script>
 
-<form method="post" use:enhance={enhance_function} class="w-full max-w-md prose">
-	<h2>Adding new product</h2>
+<form method="post" use:enhance={enhance_function} class="grid">
 	<div class="form-control">
 		<label class="label" for="name">
 			<span class="label-text">Product name</span>
@@ -74,25 +73,24 @@
 
 	<br />
 	<button disabled={loading} class="btn btn-block" class:loading>add product</button>
-	<br />
-	{#if form?.error}
-		<br />
-		<div class="alert alert-error shadow-lg">
-			<div>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="stroke-current flex-shrink-0 h-6 w-6"
-					fill="none"
-					viewBox="0 0 24 24"
-					><path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-					/></svg
-				>
-				<span>{form.error}</span>
-			</div>
-		</div>
-	{/if}
 </form>
+{#if form?.error}
+	<br />
+	<div class="alert alert-error shadow-lg">
+		<div>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="stroke-current flex-shrink-0 h-6 w-6"
+				fill="none"
+				viewBox="0 0 24 24"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+				/></svg
+			>
+			<span>{form.error}</span>
+		</div>
+	</div>
+{/if}
