701bc2c5b530a7a8e4c9dd996958d2ca5daa2a88
Don Gumayagay <dngumayagay@gmail.com>
2023-02-14 12:45:29 +0800

    add variant supply

--- src/routes/(admin)/admin/products/[product_id]/variants/[variant_id]/supplies/add/+page.server.ts
+++ src/routes/(admin)/admin/products/[product_id]/variants/[variant_id]/supplies/add/+page.server.ts
@@ -1,11 +1,9 @@
 import { fail, redirect } from '@sveltejs/kit';
 import type { PageServerLoad, Actions } from './$types';
 
-export const load = (async ({ locals, params }) => {
+export const load = (async ({ locals }) => {
 
     return {
-        product: await (await locals.supabaseClient.from("products").select('*').eq("id", params.product_id).limit(1).single()).data,
-        variant: await (await locals.supabaseClient.from("product_variants").select('*').eq("id", params.variant_id).limit(1).single()).data,
         supply_options: await (await locals.supabaseClient.from('supplies').select('name').limit(10)).data ?? []
     };
 }) satisfies PageServerLoad;
@@ -14,23 +12,23 @@ export const load = (async ({ locals, params }) => {
 export const actions: Actions = {
     default: async ({ request, locals, params }) => {
         const body = Object.fromEntries(await request.formData())
+        const variant_id = Number(params.variant_id)
+        const amount_use = Number(body.amount_use.toString())
+        if (Number.isNaN(amount_use)) return fail(400, { error: 'invalid input detected, please try again' })
+
 
         const supply_id = await (await locals.supabaseClient.from('supplies').select('id').eq('name', body.supply_name.toString()).limit(1).single()).data?.id
         if (!supply_id) return fail(404, { error: "supply you entered don't exist" })
 
-        const variant_id = Number(body.variant_id.toString())
-        const amount_use = Number(body.amount_use.toString())
-        if (Number.isNaN(variant_id) || Number.isNaN(amount_use))
-            return fail(400, { error: 'invalid input detected, please try again' })
+
 
 
         const variant_supply = {
-            id: `variant-${variant_id}-supply-${supply_id}`,
+            id: `variant-${params.variant_id}-supply-${supply_id}`,
             variant_id,
             supply_id,
             amount_use
         }
-
 
 
         const { error: err } = await locals.supabaseClient.from('variant_supply').insert(variant_supply)
--- src/routes/(admin)/admin/products/[product_id]/variants/[variant_id]/supplies/add/+page.svelte
+++ src/routes/(admin)/admin/products/[product_id]/variants/[variant_id]/supplies/add/+page.svelte
@@ -47,71 +47,63 @@
 	};
 </script>
 
-{#if product && variant}
-	<form method="post" class="w-full max-w-md prose" use:enhance={enhance_function}>
-		<h2 class="uppercase">Adding supply for product variant {variant.name}</h2>
-		<input type="hidden" name="variant_id" value={variant.id} />
-		<div class="form-control">
-			<label class="label" for="supply_name">
-				<span class="label-text">Supply name</span>
-			</label>
-			<datalist id="supply-options">
-				{#each supply_options as supply}
-					<option class=" btn" value={supply.name} />
-				{/each}
-			</datalist>
+<form method="post" class="grid" use:enhance={enhance_function}>
+	<div class="form-control">
+		<label class="label" for="supply_name">
+			<span class="label-text">Supply name</span>
+		</label>
+		<datalist id="supply-options">
+			{#each supply_options as supply}
+				<option class=" btn" value={supply.name} />
+			{/each}
+		</datalist>
+		<input
+			autoComplete="on"
+			list="supply-options"
+			class="input input-bordered"
+			placeholder="Search supply..."
+			on:keyup={debounce}
+			required
+			name="supply_name"
+		/>
+	</div>
+	<div class="form-control">
+		<label class="label" for="amount_use">
+			<span class="label-text">Supply amount will consume</span>
+		</label>
+		<label class="input-group">
 			<input
-				autoComplete="on"
-				list="supply-options"
-				class="input input-bordered"
-				placeholder="Search supply..."
-				on:keyup={debounce}
+				type="number"
+				name="amount_use"
+				placeholder="100"
+				class="input input-bordered flex-1"
 				required
-				name="supply_name"
+				min="1"
 			/>
+			<span>unit</span>
+		</label>
+	</div>
+	<br />
+	<button class="btn btn-block" class:loading disabled={loading}
+		>Add Supply for product variant</button
+	>
+</form>
+{#if form?.error}
+	<div class="alert alert-error shadow-lg">
+		<div>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="stroke-current flex-shrink-0 h-6 w-6"
+				fill="none"
+				viewBox="0 0 24 24"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+				/></svg
+			>
+			<span>{form.error}</span>
 		</div>
-		<div class="form-control">
-			<label class="label" for="amount_use">
-				<span class="label-text">Supply amount will consume</span>
-			</label>
-			<label class="input-group">
-				<input
-					type="number"
-					name="amount_use"
-					placeholder="100"
-					class="input input-bordered flex-1"
-					required
-					min="1"
-				/>
-				<span>unit</span>
-			</label>
-		</div>
-		<br />
-		<button class="btn btn-block" class:loading disabled={loading}
-			>Add Supply for product variant</button
-		>
-		<br />
-		{#if form?.error}
-			<br />
-			<div class="alert alert-error shadow-lg">
-				<div>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="stroke-current flex-shrink-0 h-6 w-6"
-						fill="none"
-						viewBox="0 0 24 24"
-						><path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-						/></svg
-					>
-					<span>{form.error}</span>
-				</div>
-			</div>
-		{/if}
-	</form>
-{:else}
-	<NotFound />
+	</div>
 {/if}
