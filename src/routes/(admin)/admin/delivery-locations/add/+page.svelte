46b39e348c08e1a51374c4809f4d116c6fea3df0
Don Gumayagay <dngumayagay@gmail.com>
2023-02-15 11:28:50 +0800

    done

--- src/routes/(admin)/admin/delivery-locations/add/+page.svelte
+++ src/routes/(admin)/admin/delivery-locations/add/+page.svelte
@@ -14,7 +14,7 @@
 	};
 </script>
 
-<form method="post" use:enhance={enhance_function} class="w-full max-w-md prose">
+<form method="post" use:enhance={enhance_function} class="grid">
 	<h2>Adding delivery location</h2>
 	<div class="form-control">
 		<label class="label" for="unit">
@@ -67,24 +67,23 @@
 	</div>
 	<br />
 	<button disabled={loading} class="btn btn-block" class:loading>Save new supply</button>
-	{#if form?.error}
-		<br />
-		<div class="alert alert-error shadow-lg">
-			<div>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="stroke-current flex-shrink-0 h-6 w-6"
-					fill="none"
-					viewBox="0 0 24 24"
-					><path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-					/></svg
-				>
-				<span>{form.error}</span>
-			</div>
-		</div>
-	{/if}
 </form>
+{#if form?.error}
+	<div class="alert alert-error shadow-lg">
+		<div>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="stroke-current flex-shrink-0 h-6 w-6"
+				fill="none"
+				viewBox="0 0 24 24"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+				/></svg
+			>
+			<span>{form.error}</span>
+		</div>
+	</div>
+{/if}
