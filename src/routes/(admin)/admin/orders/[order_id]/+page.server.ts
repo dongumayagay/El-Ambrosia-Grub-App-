8cd419910df84e1c9bdf59db7d33561c26dd6c03
Don Gumayagay <dngumayagay@gmail.com>
2023-02-18 17:16:21 +0800

    clean load

--- src/routes/(admin)/admin/orders/[order_id]/+page.server.ts
+++ src/routes/(admin)/admin/orders/[order_id]/+page.server.ts
@@ -1,44 +1,21 @@
-import { Order_States } from '$lib/misc/constants';
-import { i } from '$lib/payment/xendit.server';
-import { error, fail, redirect } from '@sveltejs/kit';
+
+import { error, fail } from '@sveltejs/kit';
 import type { PageServerLoad, Actions } from './$types';
 
 export const load = (async ({ locals, params }) => {
 
-    let { data: order, error: err_order } = await locals.supabaseClient.from('orders').select('*,order_address(*)').eq('id', params.order_id).limit(1).single()
-    if (!order || err_order) {
-        console.log(err_order)
-        throw error(404, JSON.stringify(err_order, null, 2))
-    }
-
-    if (order.status === 0 && order.invoice_id !== null) {
-        const resp = await i.getInvoice({ invoiceID: order.invoice_id })
-        // @ts-ignore
-        if (resp.status === 'PAID' || resp.status === 'SETTLED') {
-            const { data: updated_order, error: err_update_order_status } = await locals.supabaseClient.from('orders').update({ status: 1 }).eq('id', order.id).select('*,order_address(*)').order('created_at').limit(1).single()
-            order = updated_order
-            console.log(err_update_order_status)
-            throw error(500, JSON.stringify(err_update_order_status))
-        }
-    }
-
-    const { data: order_items, error: err_order_items } = await locals.supabaseClient.from('order_items').select('*,products(*),product_variants(*)').eq('order_id', params.order_id)
-    if (order_items === null || err_order_items) {
-        console.log(err_order_items)
-        throw error(500, JSON.stringify(err_order_items))
-    }
-
 
 
     return {
-        order,
-        order_items
+        order: await (await locals.supabaseClient.from('orders').select('*,order_address(*)').eq('id', params.order_id).limit(1).single()).data,
+        order_items: await (await locals.supabaseClient.from('order_items').select('*,products(*),product_variants(*)').eq('order_id', params.order_id)).data
     };
 }) satisfies PageServerLoad;
 
 export const actions: Actions = {
 
     default: async ({ locals, params, request }) => {
+
         const data = await request.formData()
         const next_status = Number(data.get('status'))
         if (isNaN(next_status)) return fail(404)
