13a65d4c1a8537c9213ec4f1f51ba03b681e5c21
Don Gumayagay <dngumayagay@gmail.com>
2023-02-24 13:27:02 +0800

    let see

--- tests/test.ts
+++ tests/test.ts
@@ -2,5 +2,6 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
+	console.log(page.url())
 	await expect(page.getByRole('heading', { name: 'El Ambrosia' })).toBeVisible();
 });
