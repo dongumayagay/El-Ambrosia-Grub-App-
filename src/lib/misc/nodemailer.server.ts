d6ef77f875eb89cb97bbde2aa8546e4dc40bd328
Don Gumayagay <dngumayagay@gmail.com>
2023-02-28 10:19:26 +0800

    add gmail

--- src/lib/misc/nodemailer.server.ts
+++ src/lib/misc/nodemailer.server.ts
@@ -1,18 +1,24 @@
 import * as nodemailer from 'nodemailer';
-
+import { GMAIL_USER, GMAIL_PASS } from "$env/static/private"
+import type Mail from 'nodemailer/lib/mailer';
 const transporter = nodemailer.createTransport({
-    host: 'smtp.example.com',
-    port: 587,
-    secure: false,
+    // host: 'smtp.example.com',
+    // port: 587,
+    // secure: false,
+    // auth: {
+    //     user: 'user@example.com',
+    //     pass: 'password',
+    // },
+    service: "gmail",
     auth: {
-        user: 'user@example.com',
-        pass: 'password',
-    },
+        user: GMAIL_USER,
+        pass: GMAIL_PASS
+    }
 });
 
-const mailOptions = {
-    from: 'sender@example.com',
-    to: 'recipient@example.com',
+const mailOptions: Mail.Options = {
+
+    to: 'dngumayagay@gmail.com',
     subject: 'Test email',
     text: 'This is a test email sent using Nodemailer.',
 };
