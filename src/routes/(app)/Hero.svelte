c17602746eee71d55aa30e2f661ac6d0f6125b22
Don Gumayagay <dngumayagay@gmail.com>
2023-03-08 10:34:14 +0800

    add cta button on hero

--- src/routes/(app)/Hero.svelte
+++ src/routes/(app)/Hero.svelte
@@ -32,29 +32,47 @@
 				of flavor today and discover the authentic taste of shawarma at El Ambrosia.
 			</p>
 			<br />
-			<div class="flex gap-2 items-center text-green-600">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="w-10 h-10"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
-					/>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
-					/>
-				</svg>
-				<div>
-					<p>Brgy. Navarro,</p>
-					<p>Gen. Trias, Cavite</p>
+			<div class="flex justify-between">
+				<a href="/products" class="btn btn-primary gap-2"
+					>See Products <svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-6 h-6 animate-side2side"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+						/>
+					</svg>
+				</a>
+				<div class="flex gap-2 items-center text-green-600">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-10 h-10"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+						/>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+						/>
+					</svg>
+					<div>
+						<p>Brgy. Navarro,</p>
+						<p>Gen. Trias, Cavite</p>
+					</div>
 				</div>
 			</div>
 		</div>
