7cce8cb98f39f4eb907f37456b30197acf608550
Don Gumayagay <dngumayagay@gmail.com>
2023-03-10 15:23:39 +0800

    remove import

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
-	import { send_email } from '$lib/misc/utils';
 	// import type { PageData } from './$types';
 
 	// export let data: PageData;
 
 	async function clickHandler() {
 		console.log(
-			await send_email({ to: 'dngumayagay@gmail.com', subject: 'Subject', html: `hello world` })
+			'eh'
 		);
 	}
 </script>
