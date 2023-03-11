84dfa033e78e3ad4b790a947061d8685c65e216f
Don Gumayagay <dngumayagay@gmail.com>
2023-03-11 20:05:30 +0800

    register cron

--- src/routes/api/cron/delete-expired-otp/+server.ts
+++ src/routes/api/cron/delete-expired-otp/+server.ts
@@ -1,6 +1,12 @@
+import { error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const DELETE: RequestHandler = async ({ locals }) => {
-    // locals.supabaseAdmin
-    return new Response();
+    try {
+        await locals.supabaseClient.rpc('delete_expired_otp_codes')
+        return new Response();
+    } catch (err) {
+        console.error(err)
+        throw error(500)
+    }
 };
