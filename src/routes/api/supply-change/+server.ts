df3aaa11351f608b0be7a03a7c380f62f75e3795
Don Gumayagay <dngumayagay@gmail.com>
2023-03-11 23:10:05 +0800

    test webhook

---
+++ src/routes/api/supply-change/+server.ts
@@ -0,0 +1,10 @@
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+
+export const POST: RequestHandler = async ({ request }) => {
+    const x_callback_token = request.headers.get('x-callback-token')
+    console.log(x_callback_token)
+    const data = request.json()
+    console.log(data)
+    return json(data);
+};
