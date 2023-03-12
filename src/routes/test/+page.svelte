facdc2b25340fdfaf82902364cf000c104d2c97d
Don Gumayagay <dngumayagay@gmail.com>
2023-03-13 07:45:06 +0800

    display map

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import L from 'leaflet';
+	import 'leaflet/dist/leaflet.css';
+
+	let map: L.Map;
+
+	onMount(() => {
+		map = L.map('map').setView([14.386682, 120.889359], 15);
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+	});
+</script>
+
+<div id="map" />
+
+<style>
+	#map {
+		height: 500px;
+	}
+</style>
