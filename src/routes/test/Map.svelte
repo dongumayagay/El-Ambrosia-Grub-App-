6c6438dbf7c601d85aa35dca0a6edfc645f35a2d
Don Gumayagay <dngumayagay@gmail.com>
2023-03-13 11:12:26 +0800

    map with auto gps current location

--- src/routes/test/Map.svelte
+++ src/routes/test/Map.svelte
@@ -2,14 +2,30 @@
 	import { onMount } from 'svelte';
 	import L from 'leaflet';
 	import 'leaflet/dist/leaflet.css';
+	import 'leaflet.locatecontrol/dist/L.Control.Locate.min.css';
+	import { browser } from '$app/environment';
 
 	let map_container: HTMLDivElement;
+	let map: L.Map;
 
 	onMount(() => {
-		L.map(map_container)
+		map = L.map(map_container)
 			.setView([14.386682, 120.889359], 15)
 			.addLayer(L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'));
+
+		return () => map.remove();
 	});
+
+	async function addLocateControl() {
+		if (browser) {
+			await import('leaflet.locatecontrol');
+			L.control.locate().addTo(map);
+		}
+	}
+
+	$: if (browser) {
+		addLocateControl();
+	}
 </script>
 
 <div id="map" bind:this={map_container} />
