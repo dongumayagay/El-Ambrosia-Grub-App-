12a1f1711890bc1fc5fd85c781116d7de6a28aae
Don Gumayagay <dngumayagay@gmail.com>
2023-03-15 02:19:20 +0800

    marker onclick

--- src/routes/test/Map.svelte
+++ src/routes/test/Map.svelte
@@ -5,16 +5,26 @@
 
 	async function setup_map(map_container: HTMLElement) {
 		const L = await import('leaflet');
+		await import('leaflet/dist/leaflet.css');
+		const EL_AMBROSIA_LOC = L.latLng([14.386682, 120.889359]);
+
 		const map = L.map(map_container)
-			.setView([14.386682, 120.889359], 19)
+			.setView(EL_AMBROSIA_LOC, 19)
 			.addLayer(L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19 }));
+		let marker: L.Marker | null = null;
+
+		map.on('click', (e) => set_marker(e.latlng));
+
+		function set_marker(latlng: L.LatLng) {
+			if (marker) map.removeLayer(marker);
+			marker = L.marker(latlng).addTo(map);
+		}
 	}
 </script>
 
 <main use:setup />
 
 <style>
-	@import 'leaflet/dist/leaflet.css';
 	main {
 		height: 500px;
 	}
