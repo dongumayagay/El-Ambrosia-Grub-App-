815d25d66a442b4153b0853a689c472ef702a14e
Don Gumayagay <dngumayagay@gmail.com>
2023-03-15 01:18:14 +0800

    display map

--- src/routes/test/Map.svelte
+++ src/routes/test/Map.svelte
@@ -1,1 +1,12 @@
-test
+<script lang="ts">
+	import * as L from 'leaflet';
+	import 'leaflet/dist/leaflet.css';
+
+	function set_map(map_container: HTMLElement) {
+		const map = L.map(map_container)
+			.setView([14.386682, 120.889359], 19)
+			.addLayer(L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19 }));
+	}
+</script>
+
+<div style="height:500px;" use:set_map />
