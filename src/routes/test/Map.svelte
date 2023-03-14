bab844ecd8f9a862e91114a9a5bcaf39b0a1fa47
Don Gumayagay <dngumayagay@gmail.com>
2023-03-14 17:33:15 +0800

    test deploy

--- src/routes/test/Map.svelte
+++ src/routes/test/Map.svelte
@@ -3,44 +3,48 @@
 	import L from 'leaflet';
 	import 'leaflet/dist/leaflet.css';
 	import 'leaflet.locatecontrol/dist/L.Control.Locate.min.css';
+	import 'leaflet.locatecontrol';
 	import { browser } from '$app/environment';
 
-	let map_container: HTMLDivElement;
-	let map: L.Map;
-	let marker: L.Marker;
+	// onMount(() => {
+	// 	map = L.map(map_container, {})
+	// 		.setView([14.386682, 120.889359], 18)
+	// 		.addLayer(L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19 }));
 
-	onMount(() => {
-		map = L.map(map_container, {})
+	// 	map.on('click', (event) => {
+	// 		if (marker) {
+	// 			marker.setLatLng(event.latlng);
+	// 		} else {
+	// 			marker = L.marker(event.latlng).addTo(map);
+	// 		}
+	// 	});
+
+	// 	return () => map.remove();
+	// });
+
+	function set_map(map_container: HTMLElement) {
+		const map = L.map(map_container, {})
 			.setView([14.386682, 120.889359], 18)
 			.addLayer(L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19 }));
 
-		map.on('click', (event) => {
-			if (marker) {
-				marker.setLatLng(event.latlng);
-			} else {
-				marker = L.marker(event.latlng).addTo(map);
-			}
-		});
-
-		return () => map.remove();
-	});
-
-	async function addLocateControl() {
-		if (browser) {
-			await import('leaflet.locatecontrol');
-			const lc = L.control.locate({ drawMarker: true }).addTo(map);
-		}
+		// map.on('click', (event) => {
+		// 	if (marker) {
+		// 		marker.setLatLng(event.latlng);
+		// 	} else {
+		// 		marker = L.marker(event.latlng).addTo(map);
+		// 	}
+		// });
 	}
 
-	$: if (browser) {
-		addLocateControl();
-	}
+	// async function addLocateControl() {
+	// 	if (browser) {
+	// 		const lc = L.control.locate({ drawMarker: true }).addTo(map);
+	// 	}
+	// }
+
+	// $: if (browser) {
+	// 	addLocateControl();
+	// }
 </script>
 
-<div style="height:500px;" id="map" bind:this={map_container} />
-
-<style>
-	#map {
-		height: 500px;
-	}
-</style>
+<div style="height:500px;" use:set_map />
