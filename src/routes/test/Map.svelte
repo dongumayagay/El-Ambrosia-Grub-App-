1309ab4d05beb39bc8e5daa37fcda73ec504f5cf
Don Gumayagay <dngumayagay@gmail.com>
2023-03-16 12:21:36 +0800

    test route deploy

--- src/routes/test/Map.svelte
+++ src/routes/test/Map.svelte
@@ -10,37 +10,63 @@
 	}
 
 	async function setup_map(map_container: HTMLElement) {
+		// import leaflet dynamically since its a heavy library
 		const L = await import('leaflet');
+		await import('leaflet-routing-machine');
 
+		// define RoutingControl type
+		type RoutingControl = ReturnType<typeof L.Routing.control>;
+
+		// business location
 		const EL_AMBROSIA_LOC = L.latLng([14.386682, 120.889359]);
+
+		// map init
 		const map = L.map(map_container)
 			.setView(EL_AMBROSIA_LOC, 19)
 			.addLayer(L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19 }));
 
-		let marker: L.Marker | null = null;
-
+		// import and add locate control
 		import('leaflet.locatecontrol').then(() => L.control.locate().addTo(map));
 
-		await import('leaflet-routing-machine');
+		// let marker: L.Marker | undefined;
+		let routing_control: RoutingControl | undefined;
 
 		map.on('click', (e) => set_marker(e.latlng));
 		map.on('locationfound', (e) => set_marker(e.latlng));
 
 		function set_marker(latlng: L.LatLng) {
-			if (marker) marker.setLatLng(latlng);
-			else
-				marker = L.marker(latlng, {
-					icon: L.icon({
-						iconUrl: markerIcon,
-						shadowUrl: markerShadow
-					})
+			// if (marker) marker.setLatLng(latlng);
+			// else
+			// 	marker = L.marker(latlng, {
+			// 		icon: L.icon({
+			// 			iconUrl: markerIcon,
+			// 			shadowUrl: markerShadow
+			// 		})
+			// 	}).addTo(map);
+
+			// after the adding the marker
+			// draw route between EL_AMBROSIA_LOC to marker
+			if (!routing_control) {
+				// initialize routing control once the library has been loaded
+				routing_control = L.Routing.control({
+					waypoints: [L.Routing.waypoint(EL_AMBROSIA_LOC), L.Routing.waypoint(latlng)]
+					// routeWhileDragging: true,
+					// fitSelectedRoutes: false
 				}).addTo(map);
+			} else {
+				// update routing control waypoints
+				routing_control.setWaypoints([
+					L.Routing.waypoint(EL_AMBROSIA_LOC),
+					L.Routing.waypoint(latlng)
+				]);
+			}
 		}
 	}
 </script>
 
 <main use:setup />
 
+<!--  -->
 <style>
 	main {
 		height: 500px;
