8838030e4d078ab478d370ba97ded72ffb7adc5f
Don Gumayagay <dngumayagay@gmail.com>
2023-03-17 17:13:22 +0800

    promise all

--- src/routes/test/Map.svelte
+++ src/routes/test/Map.svelte
@@ -11,9 +11,13 @@
 
 	async function setup_map(map_container: HTMLElement) {
 		// import leaflet dynamically since its a heavy library
-		const L = await import('leaflet');
-		await import('leaflet-routing-machine');
-
+		// const L = await import('leaflet');
+		// await import('leaflet-routing-machine');
+		const [L] = await Promise.all([
+			import('leaflet'),
+			import('leaflet-routing-machine'),
+			import('leaflet.locatecontrol')
+		]);
 		// define RoutingControl type
 		type RoutingControl = ReturnType<typeof L.Routing.control>;
 
@@ -26,40 +30,40 @@
 			.addLayer(L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19 }));
 
 		// import and add locate control
-		import('leaflet.locatecontrol').then(() => L.control.locate().addTo(map));
+		// import('leaflet.locatecontrol').then(() => L.control.locate().addTo(map));
 
-		// let marker: L.Marker | undefined;
-		let routing_control: RoutingControl | undefined;
+		let marker: L.Marker | undefined;
+		// let routing_control: RoutingControl | undefined;
 
 		map.on('click', (e) => set_marker(e.latlng));
 		map.on('locationfound', (e) => set_marker(e.latlng));
 
 		function set_marker(latlng: L.LatLng) {
-			// if (marker) marker.setLatLng(latlng);
-			// else
-			// 	marker = L.marker(latlng, {
-			// 		icon: L.icon({
-			// 			iconUrl: markerIcon,
-			// 			shadowUrl: markerShadow
-			// 		})
-			// 	}).addTo(map);
+			if (marker) marker.setLatLng(latlng);
+			else
+				marker = L.marker(latlng, {
+					icon: L.icon({
+						iconUrl: markerIcon,
+						shadowUrl: markerShadow
+					})
+				}).addTo(map);
 
 			// after the adding the marker
 			// draw route between EL_AMBROSIA_LOC to marker
-			if (!routing_control) {
-				// initialize routing control once the library has been loaded
-				routing_control = L.Routing.control({
-					waypoints: [L.Routing.waypoint(EL_AMBROSIA_LOC), L.Routing.waypoint(latlng)]
-					// routeWhileDragging: true,
-					// fitSelectedRoutes: false
-				}).addTo(map);
-			} else {
-				// update routing control waypoints
-				routing_control.setWaypoints([
-					L.Routing.waypoint(EL_AMBROSIA_LOC),
-					L.Routing.waypoint(latlng)
-				]);
-			}
+			// if (!routing_control) {
+			// 	// initialize routing control once the library has been loaded
+			// 	routing_control = L.Routing.control({
+			// 		waypoints: [L.Routing.waypoint(EL_AMBROSIA_LOC), L.Routing.waypoint(latlng)]
+			// 		// routeWhileDragging: true,
+			// 		// fitSelectedRoutes: false
+			// 	}).addTo(map);
+			// } else {
+			// 	// update routing control waypoints
+			// 	routing_control.setWaypoints([
+			// 		L.Routing.waypoint(EL_AMBROSIA_LOC),
+			// 		L.Routing.waypoint(latlng)
+			// 	]);
+			// }
 		}
 	}
 </script>
